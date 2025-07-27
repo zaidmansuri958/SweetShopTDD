79b4e56131480163f11513f02dacb16b3c53000a
Zaid <firozam196@gmail.com>
2025-07-27 08:55:41 +0530

    test(auth): ensure duplicate email registration fails

--- Backend/src/tests/auth.test.js
+++ Backend/src/tests/auth.test.js
@@ -1,32 +1,52 @@
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
 
 import request from 'supertest';
 import app from '../../app.js';
 import mongoose from 'mongoose';
-import connectDB from '../config/db.js'; 
+import connectDB from '../config/db.js';
+import User from '../models/User.js';
+
 beforeAll(async () => {
-  await connectDB(); 
+  await connectDB();
 });
 
-describe('Auth API', () => {
-  it(
-    'should register a user',
-    async () => {
-      const res = await request(app).post('/api/auth/register').send({
-        name: 'Zaid',
-        email: 'zaid@example.com',
-        password: '123456',
-      });
-
-      expect(res.statusCode).toBe(201);
-      expect(res.body).toHaveProperty('token');
-      expect(res.body.user.email).toBe('zaid@example.com');
-    },
-    10000 // timeout
-  );
+// ❌ NOT afterEach
+// ✅ Clean before every test
+beforeEach(async () => {
+  await User.deleteMany({});
 });
 
 afterAll(async () => {
   await mongoose.connection.close();
+});
+
+describe('Auth API', () => {
+  it('should register a user', async () => {
+    const res = await request(app).post('/api/auth/register').send({
+      name: 'Zaid',
+      email: 'zaid@example.com',
+      password: '123456',
+    });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty('token');
+    expect(res.body.user.email).toBe('zaid@example.com');
+  });
+
+  it('should not register same email twice', async () => {
+    await request(app).post('/api/auth/register').send({
+      name: 'Zaid',
+      email: 'repeat@example.com',
+      password: '123456',
+    });
+
+    const res = await request(app).post('/api/auth/register').send({
+      name: 'Zaid2',
+      email: 'repeat@example.com',
+      password: 'abcdef',
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
 });
